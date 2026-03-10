a8046ded1e5e0363925a849d42dae613cb9dce02
JEP (Judgment Event Protocol) <10815391+schchit@users.noreply.github.com>
2026-03-10 21:39:58 +0800

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    }
+  }
+};
